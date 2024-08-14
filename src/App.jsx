72f9ed203fa9fca15c0f0c9937c488543103eb4a
Adam Konber <konber3@gmail.com>
2024-08-14 13:09:48 -0400

    Changed application from single page to multiple page using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,50 @@
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
 
-import Navbar from './components/Navbar/navbar';
-import Intro from './components/Intro/intro';
+import Navbar from './components/Navbar/Navbar';
+import Intro from './components/Intro/Intro';
 import About from './components/About/About';
 import Portfolio from './components/Portfolio/Portfolio';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 
-
-
 function App() {
-
-
   return (
-    
+    <Router>
       <div className="App">
         <Navbar />
-        <Intro />
-        <About />
-        <Portfolio />
-        <Contact />
-        <Footer />
+        <MainContent />
       </div>
-   
-  )
+    </Router>
+  );
 }
 
-export default App
+function MainContent() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageWrapper><Intro /></PageWrapper>} />
+        <Route path="/about" element={<PageWrapper><About /></PageWrapper>} />
+        <Route path="/portfolio" element={<PageWrapper><Portfolio /></PageWrapper>} />
+        <Route path="/contact" element={<PageWrapper><Contact /></PageWrapper>} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+function PageWrapper({ children }) {
+  return (
+    <motion.div
+      initial={{ x: '100%' }}
+      animate={{ x: 0 }}
+      exit={{ x: '-100%' }}
+      transition={{ type: 'tween', duration: 0.5 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default App;
