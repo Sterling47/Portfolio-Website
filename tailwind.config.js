6392dea5f454e63eafde2a547daa910de337a59e
Adam Konber <konber3@gmail.com>
2025-01-29 10:51:18 -0700

    updates to tailwind config to handle custom box-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        shadowGray: 'rgba(78, 78, 78, 0.9)',
+        shadowGray: 'rgba(190, 190, 190, .321)',
       },
 
       animation: {
@@ -14,6 +14,7 @@ module.exports = {
         scrollRight: 'scrollRight 50s linear infinite',
       },
       boxShadow: {
+        'gradient': '0px 5px 15px rgba(0,204,177,0.3), 0px 10px 30px rgba(123,97,255,0.3), 0px 15px 45px rgba(255,196,20,0.3), 0px 20px 60px rgba(28,160,251,0.3)',
         'inset-orange': 'inset 0 0 10px #000000',
         'inset-complex': 
           'inset 3px 3px 6px 0px rgb(204, 219, 232), inset -3px -3px 6px 1px rgba(255, 255, 255, 0.5)',
