1394d7bdfe72322749405458a6e37552cd8c69d6
Adam Konber <konber3@gmail.com>
2025-01-30 09:50:06 -0700

    updated testimonials array to have deployLink and githubLink keys

--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -43,25 +43,33 @@ const Portfolio = () => {
             quote: "A React app created with Vite",
             name: "Rancid Tomatillos",
             designation: "",
-            src: rancidProject
+            src: rancidProject,
+            deployLink: 'https://rancid-tomatillos-lilac.vercel.app/',
+            githubLink: 'https://github.com/Sterling47/RancidTomatillos',
         },
         {
             quote: "A JavaScript Application",
             name: "BeerHere - A Brewery Finder",
             designation: "",
-            src: beerHereProject
+            src: beerHereProject,
+            deployLink: 'https://beer-here.vercel.app/',
+            githubLink: 'https://github.com/Sterling47/BeerHere',
         },
         {
             quote: "A React app created with Vite",
             name: "Van Life - A Van Rental Application",
             designation: "",
-            src: VanProject
+            src: VanProject,
+            deployLink: 'https://van-life-app-two.vercel.app/',
+            githubLink: 'https://github.com/Sterling47/VanLifeApp/tree/main',
         },
         {
             quote: "A fullstack application built using React, Express.js, Knex.js, and a PostgreSQL database",
             name: "Ingredient Data Search - A Fullstack App",
             designation: "",
-            src: fullStack
+            src: fullStack,
+            deployLink: 'https://backend-stretch.vercel.app/',
+            githubLink: 'https://github.com/Sterling47/backend-stretch',
         }
     ];
     
@@ -69,8 +77,8 @@ const Portfolio = () => {
     return (
         <section id="portfolio" className="flex flex-col items-center py-10 px-6 h-full w-full ">
             <BackgroundGradient className="max-w-4xl p-6 rounded-[22px] bg-white dark:bg-zinc-900 shadow-lg">
-                <h1 className="text-4xl font-bold text-center mb-4 dark:text-white">My Portfolio</h1>
-                <p className="text-center text-gray-600 dark:text-gray-300 mb-8">
+                <h1 className="text-4xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-teal-300 via-green-500 to-teal-500 m-4">My Portfolio</h1>
+                <p className="text-center text-gray-600 font-extrabold m-8 dark:text-gray-300 mb-8">
                     A collection of projects showcasing my tecnical skills and creativity.
                 </p>
                 <AnimatedTestimonials testimonials={testimonials} />
